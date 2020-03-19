a2f98114c3c2445c9253b4275acdb132a80b3693
alex <bianca.lk.alex@outlook.com>
2020-03-19 21:57:19 +0800

    显示头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends Controller
                 $data['avatar'] = $result['path'];
             }
         }
-        $user->update($request->all());
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
