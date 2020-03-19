f1acbfb56c2119f2c3d06da5e66963a7cb8d5d1d
alex <bianca.lk.alex@outlook.com>
2020-03-19 21:48:50 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
@@ -15,7 +16,15 @@ class UsersController extends Controller
     public function edit(User $user){
         return view('users.edit',compact('user'));
     }
-    public function update(UserRequest $request,User $user){
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $data = $request->all();
+
+        if($request->avatar){
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
