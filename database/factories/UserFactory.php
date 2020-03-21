4d1f83ac6c0db29ce56c63f2445dba949e5e22de
alex <bianca.lk.alex@outlook.com>
2020-03-21 18:58:44 +0800

    又修改了第一个用户的密码

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$10$C4K4tj/zYVn9mFoxblTPrOfeFOyr/Agxa.1T2U6ywDWZAMWLhXdnO', // password
         'remember_token' => Str::random(10),
         'introduction' => $faker->sentence(),
         'created_at' => $date_time,
