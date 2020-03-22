42fd16a8a25515fdbc2fc35536a37db90d313367
alex <bianca.lk.alex@outlook.com>
2020-03-22 10:57:57 +0800

    用户可以编辑话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -56,7 +56,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '修改成功');
 	}
 
 	public function destroy(Topic $topic)
