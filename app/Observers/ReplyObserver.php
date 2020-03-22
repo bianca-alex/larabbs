2b7a83a141a783fbc5f8d50a5586d37766fe785c
alex <bianca.lk.alex@outlook.com>
2020-03-22 20:39:08 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -6,6 +6,7 @@ use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -19,10 +20,13 @@ class ReplyObserver
     {
         //
     }
+    
     public function created(Reply $reply)
     {
-        //$reply->topic->increment('reply_count', 1);
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 }
