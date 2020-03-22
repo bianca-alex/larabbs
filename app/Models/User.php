0919b4ce03c977d97ac944e09a06999977a90ef5
alex <bianca.lk.alex@outlook.com>
2020-03-22 20:57:23 +0800

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,4 +67,10 @@ class User extends Authenticatable
     {
         return $this->id == $model->user_id;
     }
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
