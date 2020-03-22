5bae7af9db6e1047164bdb1c6f3aa1ff71acf240
alex <bianca.lk.alex@outlook.com>
2020-03-22 14:52:39 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,7 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+//use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -22,10 +23,14 @@ class TopicObserver
     public function saving(Topic $topic){
         $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
+    }
+    public function saved(Topic $topic){
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);  
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
-    }
+   }
     
 }
