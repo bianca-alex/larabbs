a3af6026ca79f666ae246d86501394242d8f583c
alex <bianca.lk.alex@outlook.com>
2020-03-23 22:00:12 +0800

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,6 +32,10 @@ class AuthServiceProvider extends ServiceProvider
             return 'App\Policies\\'.class_basename($modelClass).'Policy';
         });
 
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
         //
     }
 }
