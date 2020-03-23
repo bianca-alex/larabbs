07c5d8f9c9ed8e2835a5460ebe45a71c07f836ce
bianca.lk.alex <bianca.lk.alex@outlook.com>
2020-03-23 12:53:04 +0000

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasRoles;
     use Notifiable,MustVerifyEmailTrait;
 
     use Notifiable {
