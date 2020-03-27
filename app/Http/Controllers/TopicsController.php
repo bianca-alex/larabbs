2dbdb8197bb7f5111348895e951f8e9710e719ab
alex <bianca.lk.alex@outlook.com>
2020-03-27 22:28:51 +0800

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\TopicRequest;
 use Auth;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -18,13 +19,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Topic $topic, Request $request,User $user)
+    public function index(Topic $topic, Request $request,User $user,Link $link)
     {
 	$topics = $topic->withOrder($request->order)
 		 ->with('user', 'category')
  		 ->paginate(20);
 	$active_users = $user->getActiveUsers();
-        return view('topics.index', compact('topics','active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index', compact('topics','active_users','links'));
     }
 
     public function show(Topic $topic, Request $request)
---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Link;
+use Faker\Generator as Faker;
+
+$factory->define(Link::class, function (Faker $faker) {
+    return [
+        //
+        'title' => $faker->name,
+        'link' => $faker->url,
+    ];
+});
