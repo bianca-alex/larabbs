9196780cece12e2e43ff748de3c5a2e25c67c4ab
alex <bianca.lk.alex@outlook.com>
2020-03-29 20:41:03 +0800

    用户注册

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\Resource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,11 +23,11 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-	{
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
-                \App\Models\Link::observe(\App\Observers\LinkObserver::class);
-        //
+    {
+	\App\Models\User::observe(\App\Observers\UserObserver::class);
+	\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+	\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
+        Resource::withoutWrapping();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
     // 短信验证码
     Route::post('verificationCodes', 'VerificationCodesController@store')
         ->name('verificationCodes.store');
+    Route::post('users','UsersController@store')
+        ->name('users.store');
 });
