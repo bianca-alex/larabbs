c9b1ae3d8f885ba7b2ee6335c46f185d709c4f90
alex <bianca.lk.alex@outlook.com>
2020-03-29 21:38:46 +0800

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -7,12 +7,13 @@ use Illuminate\Support\Str;
 use Overtrue\EasySms\EasySms;
 use App\Http\Requests\Api\VerificationCodeRequest;
 use Cache;
+use Illuminate\Auth\AuthenticationException;
 
 class VerificationCodesController extends Controller
 {
     //
     public function store(VerificationCodeRequest $request, EasySms $easySms){
-        $phone = $request->phone;
+        /*$phone = $request->phone;
         if (!app()->environment('production')) {
             $code = '0918';
         } else {
@@ -37,6 +38,50 @@ class VerificationCodesController extends Controller
         return response()->json([
             'key' => $key,
              'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);*/
+        $captchaData = Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            abort(403, '图片验证码已失效');
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
+        if (!app()->environment('production')) {
+            $code = '0918';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'template' => config('easysms.gateways.aliyun.templates.register'),
+                    'data' => [
+                        'code' => $code
+                    ],
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('aliyun')->getMessage();
+                abort(500, $message ?: '短信发送异常');
+            }
+        }
+
+        $key = 'verificationCode_'.Str::random(15);
+        $expiredAt = now()->addMinutes(5);
+        // 缓存验证码 5分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
+
+        return response()->json([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
         ])->setStatusCode(201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Http\Request;
 
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
     Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function(){
+        // 图片验证码
+        Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
         // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
         Route::post('users','UsersController@store')->name('users.store');
