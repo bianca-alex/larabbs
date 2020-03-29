6720092d5258563ca3f4fd0afaa246d4e5e450c2
alex <bianca.lk.alex@outlook.com>
2020-03-29 20:57:56 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,12 @@ use Illuminate\Http\Request;
 */
 
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
-    //Route::post('verificationCodes','VerificationCodesController@store')->name('verificationCodes.store');   
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    Route::post('users','UsersController@store')
-        ->name('users.store');
+    Route::middleware('throttle:' . config('api.rate_limits.sign'))->group(function(){
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')->name('verificationCodes.store');
+        Route::post('users','UsersController@store')->name('users.store');
+    });
+    Route::middleware('throttle:' . config('api.rate_limits.access'))->group(function () {
+
+    });
 });
